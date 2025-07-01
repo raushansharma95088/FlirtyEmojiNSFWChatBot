556f9d5315005d38999ca8aa355da4b49e0bdb4e
raushansharma95088 <raushansharmaalpa@gmail.com>
2025-07-01 23:45:36 +0530

    emoji_image_map.py

---
+++ emoji_image_map.py
@@ -0,0 +1,37 @@
+emoji_image_map = {
+    "ðŸ˜‰": [  # Wink / Happy
+        "https://files.catbox.moe/your_happy1.jpg",
+        "https://files.catbox.moe/your_happy2.jpg",
+        "https://files.catbox.moe/your_happy3.jpg"
+    ],
+    "ðŸ˜": [  # Love
+        "https://files.catbox.moe/your_love1.jpg",
+        "https://files.catbox.moe/your_love2.jpg",
+        "https://files.catbox.moe/your_love3.jpg"
+    ],
+    "ðŸ˜ˆ": [  # Naughty / Bad
+        "https://files.catbox.moe/your_bad1.jpg",
+        "https://files.catbox.moe/your_bad2.jpg",
+        "https://files.catbox.moe/your_bad3.jpg"
+    ],
+    "ðŸ¥µ": [  # Hot
+        "https://files.catbox.moe/your_hot1.jpg",
+        "https://files.catbox.moe/your_hot2.jpg",
+        "https://files.catbox.moe/your_hot3.jpg"
+    ],
+    "ðŸ”¥": [  # Super Hot
+        "https://files.catbox.moe/your_superhot1.jpg",
+        "https://files.catbox.moe/your_superhot2.jpg",
+        "https://files.catbox.moe/your_superhot3.jpg"
+    ],
+    "ðŸ’¦": [  # Wet / Super Flirty
+        "https://files.catbox.moe/your_wet1.jpg",
+        "https://files.catbox.moe/your_wet2.jpg",
+        "https://files.catbox.moe/your_wet3.jpg"
+    ],
+    "ðŸ‘…": [  # Tongue Out / Extreme Naughty
+        "https://files.catbox.moe/your_tongue1.jpg",
+        "https://files.catbox.moe/your_tongue2.jpg",
+        "https://files.catbox.moe/your_tongue3.jpg"
+    ]
+}
